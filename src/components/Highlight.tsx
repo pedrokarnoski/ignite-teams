c0990cfa15457e071b87230d61f3cce1627f39b1
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-04-10 18:28:34 -0300

    feat: Highlight component

---
+++ src/components/Highlight.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+type HighlightProps = {
+  title: string;
+  subtitle: string;
+};
+
+export function Highlight({ title, subtitle }: HighlightProps) {
+  return (
+    <View className="w-full">
+      <Text className="text-center text-xl font-semibold text-white">
+        {title}
+      </Text>
+      <Text className="text-center text-lg text-gray-300">{subtitle}</Text>
+    </View>
+  );
+}
--- src/screens/Groups.tsx
+++ src/screens/Groups.tsx
@@ -1,11 +1,17 @@
 import { View } from "react-native";
 
 import { Header } from "@/components/Header";
+import { Highlight } from "@/components/Highlight";
 
 export function Groups() {
   return (
     <View className="flex-1 bg-gray-800">
-      <Header showBackButton />
+      <Header />
+
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com a sua turma!"
+      />
     </View>
   );
 }
